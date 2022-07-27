db6810bb5dd6c7cd491c1f99d7fc13d2fee70ad8
ViktarTachylaTR <viktar.tachyla@thomsonreuters.com>
2022-07-27 21:07:13 +0400

    task 5 updates

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
-
 const API_PATHS = {
-  product: 'https://1ws0yt9cx5.execute-api.eu-west-1.amazonaws.com/dev/products',
+  product:
+    'https://1ws0yt9cx5.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://rxg6uyxvki.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
